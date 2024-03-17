9a2eb120950dd5dbf8d9e4309bce861c13053516
poppyfanboy <poppyfanboy69@gmail.com>
2024-03-17 18:11:31 +0300

    Update cmd shell detection

--- lua/toggleterm/terminal.lua
+++ lua/toggleterm/terminal.lua
@@ -23,9 +23,17 @@ local mode = {
 local AUGROUP = api.nvim_create_augroup("ToggleTermBuffer", { clear = true })
 
 local is_windows = fn.has("win32") == 1
-local function is_cmd(shell) return shell:find("cmd") end
+local function is_cmd(shell)
+    return shell == "cmd" or shell == "cmd.exe"
+end
 
-local function is_pwsh(shell) return shell:find("pwsh") or shell:find("powershell") end
+local function is_pwsh(shell)
+    return
+        shell == 'pwsh' or
+        shell == 'pwsh.exe' or
+        shell == 'powershell.exe' or
+        shell == 'powershell'
+end
 
 local function get_command_sep() return is_windows and is_cmd(vim.o.shell) and "&" or ";" end
 
